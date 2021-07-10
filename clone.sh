c90bbc061bd379c02ec32b02ebe5a8a839d9cd6f
Subray <edmmyrage@gmail.com>
2021-07-11 01:36:00 +0300

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 # Kernel Clonning Script
 
-git clone https://github.com/slowtrip/kernel_realme_sm7125 -b eleven-cam RyZeN
+git clone --depth=1 https://github.com/slowtrip/kernel_realme_sm7125 -b eleven-cam RyZeN
 git clone --depth=1 https://github.com/karthik558/AnyKernel3 Anykernel
 git clone --depth=1 https://github.com/kdrag0n/proton-clang CLANG-13
 
